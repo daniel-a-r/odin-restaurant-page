f08903dbefcfe4e537733737bf089fca6c19eb46
daniel-a-r <daniel.a-r@outlook.com>
2025-01-11 00:06:26 -0500

    add base files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+import * as home from './home.js';
+import * as menu from './menu.js';
+import * as contact from './contact.js';
+
+console.log(home.greeting);
+console.log(menu.greeting);
+console.log(contact.greeting);
+
+console.log('hello world');
