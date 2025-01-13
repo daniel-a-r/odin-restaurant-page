30b26b7cc94bf5db6a25b23438351d3c165ee143
daniel-a-r <daniel.a-r@outlook.com>
2025-01-12 23:45:30 -0500

    move nested functions out of createPage to improve readability

--- src/home.js
+++ src/home.js
@@ -2,36 +2,6 @@ import './home.css';
 import * as htmlHelper from './htmlHelper.js';
 
 export const createPage = () => {
-  const createAboutSubsection = () => {
-    const about = htmlHelper.createSubSection('About Us');
-    const contentText = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Amet eu quam facilisis massa sodales lobortis nisl inceptos. Blandit venenatis volutpat accumsan nullam mauris. Tellus consectetur mus; sollicitudin adipiscing porta iaculis.';
-    const content = htmlHelper.createElem('div', 'content', contentText);
-
-    about.appendChild(content);
-    return about;
-  };
-
-  const createHoursSubSection = () => {
-    const hours = htmlHelper.createSubSection('Hours');
-    const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    daysOfWeek.forEach((day) => {
-      const dailyHours = htmlHelper.createContainer('daily-hours');
-      const dayElem = htmlHelper.createElem('span', 'day', `${day}:`);
-      const timeElem = htmlHelper.createElem('span', 'time', '9AM-9PM');
-      dailyHours.appendChild(dayElem);
-      dailyHours.appendChild(timeElem);
-      hours.appendChild(dailyHours);
-    });
-    return hours;
-  };
-
-  const createAddressSubSection = () => {
-    const address = htmlHelper.createSubSection('Address');
-    const content = htmlHelper.createElem('div', 'content', '123 Jotunheim Rd, Asgard');
-    address.appendChild(content);
-    return address;
-  };
-
   const main = document.querySelector('main');
   const title = htmlHelper.createTitle("Odin's Tavern");
   const about = createAboutSubsection();
@@ -42,4 +12,34 @@ export const createPage = () => {
 
   main.appendChild(title);
   main.appendChild(section);
+};
+
+const createAboutSubsection = () => {
+  const about = htmlHelper.createSubSection('About Us');
+  const contentText = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Amet eu quam facilisis massa sodales lobortis nisl inceptos. Blandit venenatis volutpat accumsan nullam mauris. Tellus consectetur mus; sollicitudin adipiscing porta iaculis.';
+  const content = htmlHelper.createElem('div', 'content', contentText);
+
+  about.appendChild(content);
+  return about;
+};
+
+const createHoursSubSection = () => {
+  const hours = htmlHelper.createSubSection('Hours');
+  const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  daysOfWeek.forEach((day) => {
+    const dailyHours = htmlHelper.createContainer('daily-hours');
+    const dayElem = htmlHelper.createElem('span', 'day', `${day}:`);
+    const timeElem = htmlHelper.createElem('span', 'time', '9AM-9PM');
+    dailyHours.appendChild(dayElem);
+    dailyHours.appendChild(timeElem);
+    hours.appendChild(dailyHours);
+  });
+  return hours;
+};
+
+const createAddressSubSection = () => {
+  const address = htmlHelper.createSubSection('Address');
+  const content = htmlHelper.createElem('div', 'content', '123 Jotunheim Rd, Asgard');
+  address.appendChild(content);
+  return address;
 };
